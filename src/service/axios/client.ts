84a54dfdc2188d78c67e28a91ddeb65591b37ea4
Hristian Valkov <hristiyanv@gmail.com>
2026-03-15 23:26:24 +0200

    refactor: axios client
    Signed-off-by: Hristian Valkov <hristiyanv@gmail.com>

--- src/service/axios/client.ts
+++ src/service/axios/client.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const OPENWEATHER_BASE_URL =
+  process.env.EXPO_PUBLIC_OPENWEATHER_BASE_URL ?? 'https://api.openweathermap.org/data/2.5';
+
 export const axiosClient = axios.create({
-  baseURL:
-    process.env.EXPO_PUBLIC_OPENWEATHER_BASE_URL ?? 'https://api.openweathermap.org/data/2.5',
+  baseURL: OPENWEATHER_BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
